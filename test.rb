0d880bca373dfe948485a22958e3cfdf294b3335
Sokolov Yura <funny.falcon@gmail.com>
2010-12-03 15:13:09 +0300

    (testapp) mNum removed

--- test.rb
+++ test.rb
@@ -62,7 +62,7 @@ template :index do
 EOF
 end
 
-default_params = { :meta => "{aSep: ' ', aForm: true, mNum: 10, mDec: 0}", :number => 1000000, :js => '' }
+default_params = { :meta => "{aSep: ' ', aForm: true, mDec: 0}", :number => 1000000, :js => '' }
 get '/' do
   haml :index, :locals => default_params
 end
